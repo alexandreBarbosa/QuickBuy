7563ce236e723e7dc9ee05c9c123485aae0361ec
Alexandre Barbosa <alexandre.barbosa@basis.com.br>
2020-05-03 19:36:11 -0300

    aula 40

--- QuickBuy.Dominio/Entidades/Usuario.cs
+++ QuickBuy.Dominio/Entidades/Usuario.cs
@@ -6,7 +6,7 @@ namespace QuickBuy.Dominio.Entidades
 {
     public class Usuario : Entidade
     {
-        public int id { get; set; }
+        public int Id { get; set; }
         public string Email { get; set; }
         public string Senha { get; set; }
         public string Nome { get; set; }
@@ -14,7 +14,7 @@ namespace QuickBuy.Dominio.Entidades
         /// <summary>
         /// Um Usuario pode ter nenhum ou muitos pedidos
         /// </summary>
-        public ICollection<Pedido> Pedidos { get; set; }
+        public virtual ICollection<Pedido> Pedidos { get; set; }
 
         public override void Validate()
         {
