591c294be58748d9da245321fe3dd48deee5b5a9
Alexandre Barbosa <alexandre.barbosa@basis.com.br>
2020-05-17 23:16:50 -0300

    aula 90

---
+++ QuickBuy.Web/ClientApp/src/app/autorizacao/guarda.rotas.ts
@@ -0,0 +1,19 @@
+import { Injectable } from "@angular/core";
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from "@angular/router";
+
+@Injectable({
+  providedIn:'root'
+})
+export class GuardaRotas implements CanActivate {
+  //private router: Router;
+  constructor(private router: Router) {
+
+  }
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean  {
+    //this.router.navigate(['/entrar']);
+      //se usuario autenticado
+      return true;
+    }
+
+
+}
